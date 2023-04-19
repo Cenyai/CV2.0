fc0f379c1952a6de8181e5238da52069f567efda
Cenyai <carlosyygg@gmail.com>
2023-04-19 17:45:36 +0200

    arreglos y juntar todo en uno

---
+++ js/index.js
@@ -0,0 +1,57 @@
+window.onload=()=>{
+    document.getElementById("btn1").onclick=()=>{
+       let divContenido= document.getElementById("contenidoDatosPersonales");
+       if(divContenido.classList.contains("d-none")){
+        divContenido.classList.remove("d-none")
+       }else{
+        divContenido.classList.add("d-none")
+       }
+    }
+}
+
+window.onload=()=>{
+    document.getElementById("btn2").onclick=()=>{
+       let divContenido= document.getElementById("contenidoImportante");
+       if(divContenido.classList.contains("d-none")){
+        divContenido.classList.remove("d-none")
+       }else{
+        divContenido.classList.add("d-none")
+       }
+    }
+}
+
+
+window.onload=()=>{
+    document.getElementById("btn3").onclick=()=>{
+       let divContenido= document.getElementById("contenidoEstudios");
+       if(divContenido.classList.contains("d-none")){
+        divContenido.classList.remove("d-none")
+       }else{
+        divContenido.classList.add("d-none")
+       }
+    }
+}
+window.onload=()=>{
+    document.getElementById("btn4").onclick=()=>{
+       let divContenido= document.getElementById("contenidoExperienciaProfesional");
+       if(divContenido.classList.contains("d-none")){
+        divContenido.classList.remove("d-none")
+       }else{
+        divContenido.classList.add("d-none")
+       }
+    }
+}
+
+window.onload=()=>{
+    document.getElementById("btn5").onclick=()=>{
+       let divContenido= document.getElementById("contenidoIdiomas");
+       if(divContenido.classList.contains("d-none")){
+        divContenido.classList.remove("d-none")
+       }else{
+        divContenido.classList.add("d-none")
+       }
+    }
+}
+
+
+
