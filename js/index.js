48126cc548e24011844096c73d622b31b6c28bc6
Cenyai <carlosyygg@gmail.com>
2023-04-24 16:46:47 +0200

    cambios en js

--- js/index.js
+++ js/index.js
@@ -9,38 +9,38 @@ window.onload = () => {
     }
 
     document.getElementById("btn2").onclick = () => {
-        let divContenido = document.getElementById("contenidoImportante");
-        if (divContenido.classList.contains("d-none")) {
-            divContenido.classList.remove("d-none")
+        let divContenido = document.getElementById("importante");
+        if (divContenido.classList.contains("ocultar")) {
+            divContenido.classList.remove("ocultar")
         } else {
-            divContenido.classList.add("d-none")
+            divContenido.classList.add("ocultar")
         }
     }
 
     document.getElementById("btn3").onclick = () => {
-        let divContenido = document.getElementById("contenidoEstudios");
-        if (divContenido.classList.contains("d-none")) {
-            divContenido.classList.remove("d-none")
+        let divContenido = document.getElementById("estudios");
+        if (divContenido.classList.contains("ocultar")) {
+            divContenido.classList.remove("ocultar")
         } else {
-            divContenido.classList.add("d-none")
+            divContenido.classList.add("ocultar")
         }
     }
 
     document.getElementById("btn4").onclick = () => {
-        let divContenido = document.getElementById("contenidoExperienciaProfesional");
-        if (divContenido.classList.contains("d-none")) {
-            divContenido.classList.remove("d-none")
+        let divContenido = document.getElementById("expprof");
+        if (divContenido.classList.contains("ocultar")) {
+            divContenido.classList.remove("ocultar")
         } else {
-            divContenido.classList.add("d-none")
+            divContenido.classList.add("ocultar")
         }
     }
 
     document.getElementById("btn5").onclick = () => {
-        let divContenido = document.getElementById("contenidoIdiomas");
-        if (divContenido.classList.contains("d-none")) {
-            divContenido.classList.remove("d-none")
+        let divContenido = document.getElementById("idiomas");
+        if (divContenido.classList.contains("ocultar")) {
+            divContenido.classList.remove("ocultar")
         } else {
-            divContenido.classList.add("d-none")
+            divContenido.classList.add("ocultar")
         }
     }
 }
