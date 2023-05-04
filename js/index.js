3a246c711fd08e696169eb5ea7e10c4899eb3612
Cenyai <carlosyygg@gmail.com>
2023-05-04 16:16:28 +0200

    botones2

--- js/index.js
+++ js/index.js
@@ -45,5 +45,8 @@ window.onload = () => {
     }
 }
 
+document.getElementById("boton").addEventListener("click", function(){
+    window.location.href = "index.html"; // Reemplaza "index.html" con la URL de tu página principal
+  });
 
 
